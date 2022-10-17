ea008b802fd66a660e799bfc23ed0fe1e0f5b7b7
Devi Raju <deviraju999@gmail.com>
2022-10-17 16:03:39 -0700

    added 2 new tables and added fake data to the tables

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -7,7 +7,6 @@ end_date DATE
 
 CREATE TABLE students (
 id SERIAL PRIMARY KEY NOT NULL,
-
 name VARCHAR(255) NOT NULL,
 email VARCHAR(255),
 phone VARCHAR(32),
@@ -16,3 +15,5 @@ start_date DATE,
 end_date DATE,
 cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
 );
+
+
