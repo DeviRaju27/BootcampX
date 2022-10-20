905d42d0f932e494c4c4ebd8f52adf17df60c537
Devi Raju <deviraju999@gmail.com>
2022-10-20 12:25:27 -0700

    parameterized query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,22 +1,21 @@
 const { Pool } = require('pg');
 const [, , cohort_month] = process.argv;
+const values = [`${cohort_month}`]
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
   host: 'localhost',
   database: 'bootcampx'
-
 })
+const query = `SELECT DISTINCT teachers.name as teacher_name, cohorts.name as cohort_name
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher_name;` 
 
-pool.query(
-  `SELECT DISTINCT teachers.name as teacher_name, cohorts.name as cohort_name
-  FROM teachers
-  JOIN assistance_requests ON teacher_id = teachers.id
-  JOIN students ON student_id = students.id
-  JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name = '${cohort_month}'
-  ORDER BY teacher_name;` 
-)
+pool.query(query, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort_name}: ${user.teacher_name}`)
