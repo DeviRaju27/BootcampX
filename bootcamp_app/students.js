3bf263578c694bdd958f216c9b79d3afc868a955
Devi Raju <deviraju999@gmail.com>
2022-10-20 12:22:04 -0700

    parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,5 +1,6 @@
 const { Pool } = require('pg');
 const [, , cohort_month, limit] = process.argv;
+const values = [`%${cohort_month}%`,`${limit}`]
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -8,14 +9,16 @@ const pool = new Pool({
 
 })
 
-pool.query(
+const query = 
   `SELECT students.id as student_id, students.name as student_name, cohorts.name as cohort_name
   FROM students
   JOIN cohorts
   ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '%${cohort_month}%'
-  LIMIT ${limit};` 
-)
+  WHERE cohorts.name LIKE $1
+  LIMIT $2 ;`
+
+
+pool.query(query, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.student_name} is in ${user.cohort_name}`)
